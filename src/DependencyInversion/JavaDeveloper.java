48e92503fd5bc7bafd60d5e7ea3b8e92791ba9dc
BurghardH <burghard.hillig@googlemail.com>
2018-11-27 19:00:21 +0100

    Basic classes

---
+++ src/DependencyInversion/JavaDeveloper.java
@@ -0,0 +1,8 @@
+package DependencyInversion;
+
+public class JavaDeveloper {
+
+	public void writeJavaCode() {
+		System.out.println("Ich schreibe Java Code.");
+	}
+}
---
+++ src/DependencyInversion/JavaScriptDeveloper.java
@@ -0,0 +1,8 @@
+package DependencyInversion;
+
+public class JavaScriptDeveloper {
+
+	public void writeJavaScriptCode() {
+		System.out.println("Ich schreibe JavaScript Code.");
+	}
+}
