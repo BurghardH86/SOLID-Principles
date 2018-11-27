df30e7f2fa785e437e2a390b3d297bf33246cdcf
BurghardH <burghard.hillig@googlemail.com>
2018-11-27 07:42:51 +0100

    First version

--- src/LiskovSubstitution/Program.java
+++ src/LiskovSubstitution/Program.java
@@ -3,8 +3,17 @@ package LiskovSubstitution;
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Rectangle rec1 = new Rectangle(4, 5);
+		doSomething(rec1);
 
+	}
+
+	public static void doSomething(Rectangle rec) {
+		double height = rec.getHeight();
+		rec.setWidth(20);
+		// 20 * height
+		System.out.println("Vermuteter Flächeninhalt: " + (20 * height));
+		System.out.println("Tatsächlicher Flächeninhalt: " + rec.area());
 	}
 
 }
