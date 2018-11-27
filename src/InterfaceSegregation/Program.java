2c634ec95fa41bb7e8252ced610fa0745b958f0b
BurghardH <burghard.hillig@googlemail.com>
2018-11-27 18:53:45 +0100

    Basic interface

---
+++ src/InterfaceSegregation/Program.java
@@ -0,0 +1,10 @@
+package InterfaceSegregation;
+
+public class Program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/InterfaceSegregation/Restaurant.java
@@ -0,0 +1,10 @@
+package InterfaceSegregation;
+
+public interface Restaurant {
+	public void payCreditCard();
+	public void payCash();
+	public void paySmartphone();
+	public void customerRatingService();
+	public void takeTelephoneOrder();
+
+}
