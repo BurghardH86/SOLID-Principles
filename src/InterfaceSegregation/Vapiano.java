b31918cd628195e20889e6de072e28b746ea4c81
BurghardH <burghard.hillig@googlemail.com>
2018-11-27 18:54:51 +0100

    Additional classes

---
+++ src/InterfaceSegregation/Vapiano.java
@@ -0,0 +1,35 @@
+package InterfaceSegregation;
+
+public class Vapiano implements Restaurant{
+
+	@Override
+	public void payCreditCard() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void payCash() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void paySmartphone() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void customerRatingService() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void takeTelephoneOrder() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
