40412715fdcde0555597c170d7e69ba8433a65e3
BurghardH <burghard.hillig@googlemail.com>
2018-11-27 07:26:54 +0100

    Adjusted main

--- src/OpenClosed/Program.java
+++ src/OpenClosed/Program.java
@@ -7,10 +7,12 @@ public class Program {
 		Rectangle rec2 = new Rectangle(4, 4); //16;
 		Rectangle rec3 = new Rectangle(2, 3); //6;
 		
-		Rectangle[] recArray = {rec1, rec2, rec3};
+		Circle circle1 = new Circle(3); // 28,274
+		
+		Object[] shapes = {rec1, rec2, rec3, circle1};
 		
 		AreaCalculator ac = new AreaCalculator();
-		System.out.println("Fläche: " + ac.calculateArea(recArray));
+		System.out.println("Fläche: " + ac.calculateArea(shapes));
 	}
 
 }
